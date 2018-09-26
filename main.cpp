e5684d0617b782c39c80837512fa12a2bcec1596
Kyosuke Tawara <astrowidow@gmail.com>
2018-09-27 00:12:59 +0900

    テストイメージを共有ようにコミット

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 #include "./ProcessStrategy/ProcessStrategyContrastConversion.h"
 
 int main() {
-    //Bitmap longPhaseImage("./testImage/LongPhaseX.bmp");
-    Bitmap longPhaseImage("./testImage/lena_gray.bmp");
-    Bitmap shortPhaseImage("./testImage/lena_gray.bmp");
+    //Bitmap longPhaseImage("../testImage/LongPhaseX.bmp");
+    Bitmap longPhaseImage("../testImage/lena_gray.bmp");
+    Bitmap shortPhaseImage("../testImage/lena_gray.bmp");
     Bitmap test(512, 512, 8);
     Pixel testPix(1);
     BYTE pix_data = 0;
@@ -16,7 +16,7 @@ int main() {
             shortPhaseImage.setPixel(width, height, &testPix);
         }
     }
-    shortPhaseImage.writeBitmap("./testImage/black.bmp");
+    shortPhaseImage.writeBitmap("../testImage/black.bmp");
 
     for(UINT width = 0; width < 512; width++){
         for(UINT height = 0; height < 512; height++){
@@ -26,11 +26,11 @@ int main() {
             //printf("width = %d, height = %d, data is %x  %x  %x  %x\n", width, height, pix_data[0], pix_data[1], pix_data[2], pix_data[3]);
         }
     }
-    shortPhaseImage.writeBitmap("./testImage/testGenerateByShortPhaseImage.bmp");
-    test.writeBitmap("./testImage/testGenerateByTest.bmp");
+    shortPhaseImage.writeBitmap("../testImage/testGenerateByShortPhaseImage.bmp");
+    test.writeBitmap("../testImage/testGenerateByTest.bmp");
     // strategy patternのテスト
     ProcessStrategy* contrast_conversion = (ProcessStrategy*) new ProcessStrategyContrastConversion(&longPhaseImage, &test, 3, -150);
     contrast_conversion->executeProcessing();
-    test.writeBitmap("./testImage/contrastTest.bmp");
+    test.writeBitmap("../testImage/contrastTest.bmp");
     return 0;
 }
