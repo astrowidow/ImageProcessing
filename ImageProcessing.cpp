47ad942f651e7ffe3b07222821ebb2da82685d86
Kyosuke Tawara <astrowidow@gmail.com>
2018-10-11 23:42:03 +0900

    実行時間を追記

--- ImageProcessing.cpp
+++ ImageProcessing.cpp
@@ -25,28 +25,28 @@ ImageProcessing::ImageProcessing(UINT image_height, UINT image_width):height(ima
 // Release build
 // テンプレートパターンを使った実装だと
 // ... 512pix*512pix*8bit の画像に16*10回コントラスト変換実行
-// ... ... execution time is 1.4952 [sec]
+// ... ... execution time is 0.715390 [sec]
 // ... 512pix*512pix*8bit の画像に16*100回コントラスト変換実行
-// ... ... execution time is 14.043077 [sec]
+// ... ... execution time is 6.248881 [sec]
 // ... 512pix*512pix*8bit の画像に16*1000回コントラスト変換実行
-// ... ... execution time is 134.921463 [sec]
+// ... ... execution time is 56.505589 [sec]
 // テンプレートパターンを使わない実装だと
 // ... 512pix*512pix*8bit の画像に16*10回コントラスト変換実行
-// ... ... execution time is 0.602235 [sec]
+// ... ... execution time is 0.591836 [sec]
 // ... 512pix*512pix*8bit の画像に16*100回コントラスト変換実行
-// ... ... execution time is 5.148182 [sec]
+// ... ... execution time is 4.647976 [sec]
 // ... 512pix*512pix*8bit の画像に16*1000回コントラスト変換実行
 // ... ... execution time is 49.928997 [sec]
 // 全然差がない....
 
 void ImageProcessing::execute(){
-    //executeOpening();
-    //for(UINT row = 0; row < height; row++){
-        //for(UINT col = 0; col < width; col++){
+//    executeOpening();
+//    for(UINT row = 0; row < height; row++){
+//        for(UINT col = 0; col < width; col++){
             executeProcess();
-        //}
-    //}
-    //executeEnding();
+//        }
+//    }
+//    executeEnding();
 }
 
 
